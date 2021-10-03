5048281fe7d437bf1859ff385eaa837f73fb6f5d
fvrkn <fozkan21@ku.edu.tr>
2021-10-03 21:04:45 +0300

    PART1: Hello World! Homework

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ import math
 input_one = int(input("Please enter a number X:"))
 input_two = int(input("Please enter a number Y:"))
 print(f"Number X, Raised to the power Y is: {input_one ** input_two}")
-logValue = math.log(input_one, 2)
-print(f"Log (base 2) of X is: {logValue}")
+print(f"Log (base 2) of X is: {math.log(input_one, 2)}")
 print("Student Number: 80460")
